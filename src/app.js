9ceab4bf5673487866ef006c295c240dbc6509cf
Aaron Tan <cheeseisdisgusting@gmail.com>
2016-06-14 13:08:07 -0400

    Added credits screen

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ var ajax = require('ajax');
 var moment = require('moment');
 var Vector2 = require('vector2');
 var Feature = require('platform/feature');
+var Accel = require('ui/accel');
 var dayFinder = require('dayFinder.js');
 var periodSetter = require('periodSetter.js');
 var itemSetter = require('itemSetter.js');
@@ -529,3 +530,88 @@ function createExtraDetailWindow(e, window) {
   window.add(classRoomsText);
   window.add(centerImage);
 }
+
+Accel.config();
+//A little easter egg
+var tapped = 0;
+mainWind.on('accelTap', function(e) {
+  tapped ++;
+  console.log('Tapped the window ' + tapped + ' times!');
+  if (tapped == 10) {
+    tapped = 0;
+    var easterEgg = new UI.Window({
+      backgroundColor: 'white',
+      status: false
+    });
+    var devText = new UI.Text({
+      size: Feature.rectangle(new Vector2(Feature.resolution().x - 24, 27),
+                             new Vector2(Feature.resolution().x / 2, 27)),
+      font: 'gothic-18',
+      position: Feature.rectangle(new Vector2(12, Feature.resolution().y / 2 - 80),
+                                  new Vector2(Feature.resolution().x / 4, Feature.resolution().y / 2 - 70)),
+      text: 'MADE BY:',
+      textAlign: 'center',
+      textOverflow: 'ellipsis',
+      color: 'black'
+    });
+    var devName = new UI.Text({ //textbox for name of developer
+    size: Feature.rectangle(new Vector2(Feature.resolution().x - 24, 42),
+                           new Vector2(Feature.resolution().x * 0.75, 42)),
+    font: 'gothic-28-bold',
+    position: Feature.rectangle(new Vector2(12, Feature.resolution().y / 2 - 66),
+                                new Vector2(Feature.resolution().x * 0.125, Feature.resolution().y / 2 - 56)),
+    text: 'AARON TAN',
+    textAlign: 'center',
+    textOverflow: 'ellipsis',
+    color: 'black'
+  });
+    var centerImage = new UI.Image({
+      size: new Vector2(64, 64),
+      position: new Vector2(Feature.resolution().x / 2 - 32, Feature.resolution().y / 2 - 32),
+      image: 'images/cow',
+      compositing: 'set'
+  });
+    var captionText = new UI.Text({
+      size: Feature.rectangle(new Vector2(Feature.resolution().x - 24, 42),
+                             new Vector2(Feature.resolution().x * 0.75, 42)),
+      font: 'gothic-18',
+      position: Feature.rectangle(new Vector2(12, Feature.resolution().y / 2 - 40),
+                                  new Vector2(Feature.resolution().x * 0.125, Feature.resolution().y / 2 - 40)),
+      text: 'Here\'s a cow.',
+      textAlign: 'center',
+      textOverflow: 'ellipsis',
+      color: 'black'
+  });
+  if (Feature.rectangle()) {
+    var creditsText = new UI.Text({
+      size: Feature.rectangle(new Vector2(Feature.resolution().x - 24, 42),
+                              new Vector2(Feature.resolution().x * 0.75, 42)),
+      font: 'gothic-18',
+      position: Feature.rectangle(new Vector2(12, Feature.resolution().y / 2 + 18),
+                                  new Vector2(Feature.resolution().x * 0.125, Feature.resolution().y / 2 + 28)),
+      text: 'Icons from Open Iconic, IconArchive',
+      textAlign: 'center',
+      textOverflow: 'ellipsis',
+      color: 'black'
+    });
+    easterEgg.add(creditsText);
+  }
+    var copyrightText = new UI.Text({
+      size: Feature.rectangle(new Vector2(Feature.resolution().x - 24, 42),
+                             new Vector2(Feature.resolution().x * 0.75, 42)),
+      font: 'gothic-18',
+      position: Feature.rectangle(new Vector2(12, Feature.resolution().y / 2 + 55),
+                                  new Vector2(Feature.resolution().x * 0.125, Feature.resolution().y / 2 + 45)),
+      text: '© 2016, v0.31',
+      textAlign: 'center',
+      textOverflow: 'ellipsis',
+      color: 'black'
+  });
+    easterEgg.add(devText);
+    easterEgg.add(devName);
+    easterEgg.add(centerImage);
+    easterEgg.add(captionText);
+    easterEgg.add(copyrightText);
+    easterEgg.show();
+  }
+});
