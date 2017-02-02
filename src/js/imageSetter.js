741373655f00ed13626849566ba711d06360b5a9
Aaron Tan <cheeseisdisgusting@gmail.com>
2017-02-01 21:55:36 -0500

    Fixed class icons not appearing
    The little icons for each period should be back now!

--- src/js/imageSetter.js
+++ src/js/imageSetter.js
@@ -3,44 +3,44 @@ var exports = this.exports = {};
 // This module chooses an image based on the string that is entered. For the subjects, mostly.
 exports.setImage = function(periodName) {
   if (periodName.indexOf('SPACE') >= 0 || periodName.indexOf('EARTH') >= 0) {
-    return 'images/rocket';
+    return 'images/rocket.png';
   } else if (periodName.indexOf('CHEM') >= 0 || periodName.indexOf('SCIENCE') >= 0) {
-    return 'images/beaker';
+    return 'images/beaker.png';
   } else if (periodName.indexOf('HISTORY') >= 0) {
-    return 'images/book';
+    return 'images/book.png';
   } else if (periodName.indexOf('LAW') >= 0) {
-    return 'images/briefcase';
+    return 'images/briefcase.png';
   } else if (periodName.indexOf('ART') >= 0) {
-    return 'images/brush';
+    return 'images/brush.png';
   } else if (periodName.indexOf('BIO') >= 0) {
-    return 'images/bug';
+    return 'images/bug.png';
   } else if (periodName.indexOf('FILM') >= 0 || periodName.indexOf('VISION') >= 0) {
-    return 'images/camera';
+    return 'images/camera.png';
   } else if (periodName.indexOf('BUSI') >= 0) {
-    return 'images/dollar';
+    return 'images/dollar.png';
   } else if (periodName.indexOf('FRENCH') >= 0) {
-    return 'images/eiffel';
+    return 'images/eiffel.png';
   } else if (periodName.indexOf('GEO') >= 0) {
-    return 'images/globe';
+    return 'images/globe.png';
   } else if (periodName.indexOf('GYM') >= 0 || periodName.indexOf('OOR ED') >= 0 || periodName.indexOf('DANCE') >= 0) {
-    return 'images/heart';
+    return 'images/heart.png';
   } else if (periodName.indexOf('PHYSICS') >= 0) {
-    return 'images/undo';
+    return 'images/undo.png';
   } else if (periodName.indexOf('TECH') >= 0 || periodName.indexOf('COMPUTER') >= 0  || periodName.indexOf('ICS') >= 0) {
-    return 'images/monitor';
+    return 'images/monitor.png';
   } else if (periodName.indexOf('MUSIC') >= 0) {
-    return 'images/note';
+    return 'images/note.png';
   } else if (periodName.indexOf('ENGLISH') >= 0) {
-    return 'images/pencil';
+    return 'images/pencil.png';
   } else if (periodName.indexOf('ANTHRO') >= 0 || periodName.indexOf('PSYCH') >= 0) {
-    return 'images/person';
+    return 'images/person.png';
   } else if (periodName.indexOf('MATH') >= 0 || periodName.indexOf('CALC') >= 0 || periodName.indexOf('VEC') >= 0) {
-    return 'images/plus';
+    return 'images/plus.png';
   } else if (periodName.indexOf('TOK') >= 0 || periodName.indexOf('KNOW') >= 0) {
-    return 'images/puzzle';
+    return 'images/puzzle.png';
   } else if (periodName.indexOf('SPANISH') >= 0) {
-    return 'images/sombrero';
+    return 'images/sombrero.png';
   } else {
-    return 'images/calendar';
+    return 'images/calendar.png';
   } 
 };
